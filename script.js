6f958ac2133b499072d20b41a24686de53cb70fc
AdminC3A <supervision@casatresaguas.com>
2024-12-09 18:49:49 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,21 +3,13 @@ function onScanSuccess(decodedText, decodedResult) {
     // Mostrar el texto escaneado
     document.getElementById('result').innerText = `Código escaneado: ${decodedText}`;
 }
-  
-  // Aquí llamaremos a la función de validación en el siguiente paso
-  validateAccess(decodedText);
 // Manejar errores durante el escaneo
 function onScanError(errorMessage) {
     console.error("Error durante el escaneo: ", errorMessage);
 }
-
 // Inicializar el escáner QR
 const html5QrcodeScanner = new Html5QrcodeScanner(
     "reader", { fps: 10, qrbox: 250 }
 );
-
 // Renderizar el escáner
 html5QrcodeScanner.render(onScanSuccess, onScanError);
-
-
-startScanning();
